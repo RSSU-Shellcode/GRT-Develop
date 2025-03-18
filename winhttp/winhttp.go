595390d887e72731dabe240957a0bcbed399f81a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-18 14:46:06 +0800

    winhttp.go: replace the type of Body to any for skip this field value.

--- winhttp/winhttp.go
+++ winhttp/winhttp.go
@@ -2,23 +2,23 @@ package winhttp
 
 // Request is the defined HTTP request in win_http.h.
 type Request struct {
-	URL            string  // https://user:pass@www.example.com/test.txt
-	Headers        string  // split by "\r\n"
-	UserAgent      string  // default User-Agent
-	ProxyURL       string  // http://www.example.com:8080
-	ProxyUser      string  // proxy server username
-	ProxyPass      string  // proxy server password
-	ConnectTimeout uint32  // milliseconds, default is 60s
-	SendTimeout    uint32  // milliseconds, default is 600s
-	ReceiveTimeout uint32  // milliseconds, default is 600s
-	MaxBodySize    uint32  // zero is no limit
-	AccessType     uint8   // reference document about WinHttpOpen
-	Body           uintptr // skip this field value
+	URL            string // https://user:pass@www.example.com/test.txt
+	Headers        string // split by "\r\n"
+	UserAgent      string // default User-Agent
+	ProxyURL       string // http://www.example.com:8080
+	ProxyUser      string // proxy server username
+	ProxyPass      string // proxy server password
+	ConnectTimeout uint32 // milliseconds, default is 60s
+	SendTimeout    uint32 // milliseconds, default is 600s
+	ReceiveTimeout uint32 // milliseconds, default is 600s
+	MaxBodySize    uint32 // zero is no limit
+	AccessType     uint8  // reference document about WinHttpOpen
+	Body           any    // skip this field value
 }
 
 // Response is the defined HTTP response in win_http.h.
 type Response struct {
-	StatusCode int32   // example 200, 404
-	Headers    string  // split by "\r\n"
-	Body       uintptr // skip this field value
+	StatusCode int32  // example 200, 404
+	Headers    string // split by "\r\n"
+	Body       any    // skip this field value
 }
