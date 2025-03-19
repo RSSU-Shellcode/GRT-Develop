df75442e8e60385fabceccca4e2eb03616099afc
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-19 18:33:35 +0800

    decode_test.go: add more unit tests about Unmarshal, add test types.

--- serialization/decode_test.go
+++ serialization/decode_test.go
@@ -1,6 +1,7 @@
 package serialization
 
 import (
+	"encoding/binary"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -47,5 +48,91 @@ func TestUnmarshal(t *testing.T) {
 
 		s1.unexported = 0
 		require.Equal(t, s1, s2)
+	})
+
+	t.Run("invalid value type", func(t *testing.T) {
+		var s1 int
+		err := Unmarshal(nil, s1)
+		require.EqualError(t, err, "value must be a non-nil pointer")
+
+		err = Unmarshal(nil, &s1)
+		require.EqualError(t, err, "value must be a pointer to struct")
+	})
+
+	t.Run("invalid data length", func(t *testing.T) {
+		var s1 testStruct
+		err := Unmarshal(nil, &s1)
+		require.EqualError(t, err, "invalid data length")
+	})
+
+	t.Run("invalid magic number", func(t *testing.T) {
+		data := make([]byte, 8)
+
+		var s1 testStruct
+		err := Unmarshal(data, &s1)
+		require.EqualError(t, err, "invalid magic number")
+	})
+
+	t.Run("invalid descriptor", func(t *testing.T) {
+		data := make([]byte, 4+3)
+		binary.LittleEndian.PutUint32(data, headerMagic)
+
+		var s1 testStruct
+		err := Unmarshal(data, &s1)
+		require.EqualError(t, err, "unexpected EOF")
+	})
+
+	t.Run("invalid structure field", func(t *testing.T) {
+		data := make([]byte, 8)
+		binary.LittleEndian.PutUint32(data, headerMagic)
+
+		var s1 testStruct
+		err := Unmarshal(data, &s1)
+		require.EqualError(t, err, "invalid number of struct fields: 43")
+	})
+
+	t.Run("invalid value size", func(t *testing.T) {
+		data := []byte{
+			0xFF, 0xFF, 0xFF, 0xFF,
+			0x03, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+		}
+		s := struct {
+			Arg1 uint16
+		}{}
+
+		err := Unmarshal(data, &s)
+		require.EqualError(t, err, "failed to decode value: invalid size: 3")
+	})
+
+	t.Run("invalid raw data size", func(t *testing.T) {
+		data := []byte{
+			0xFF, 0xFF, 0xFF, 0xFF,
+			0x02, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+		}
+		s := struct {
+			Arg1 uint16
+		}{}
+
+		err := Unmarshal(data, &s)
+		require.EqualError(t, err, "failed to decode value: EOF")
+	})
+
+	t.Run("not supported field type", func(t *testing.T) {
+		data := []byte{
+			0xFF, 0xFF, 0xFF, 0xFF,
+			0x02, 0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00, 0x00,
+			0x00, 0x00,
+		}
+		s := struct {
+			Arg1 struct {
+				_ uint16
+			}
+		}{}
+
+		err := Unmarshal(data, &s)
+		require.EqualError(t, err, "failed to decode value: type of struct is not support")
 	})
 }
