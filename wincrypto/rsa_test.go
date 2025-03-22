dff4d34c55fc196e1b0ae617910bce2beaa645b4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-22 17:56:34 +0800

    rsa_test.go: add TestExportRSAPrivateKeyBlob and TestExportRSAPublicKeyBlob.

--- wincrypto/rsa_test.go
+++ wincrypto/rsa_test.go
@@ -1,1 +1,52 @@
 package wincrypto
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExportRSAPrivateKeyBlob(t *testing.T) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	t.Run("sign", func(t *testing.T) {
+		blob, err := ExportRSAPrivateKeyBlob(key, RSAKeyUsageSIGN)
+		require.NoError(t, err)
+
+		spew.Dump(blob)
+		require.Len(t, blob, 1172)
+	})
+
+	t.Run("key exchange", func(t *testing.T) {
+		blob, err := ExportRSAPrivateKeyBlob(key, RSAKeyUsageKEYX)
+		require.NoError(t, err)
+
+		spew.Dump(blob)
+		require.Len(t, blob, 1172)
+	})
+}
+
+func TestExportRSAPublicKeyBlob(t *testing.T) {
+	key, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	t.Run("sign", func(t *testing.T) {
+		blob, err := ExportRSAPublicKeyBlob(&key.PublicKey, RSAKeyUsageSIGN)
+		require.NoError(t, err)
+
+		spew.Dump(blob)
+		require.Len(t, blob, 276)
+	})
+
+	t.Run("key exchange", func(t *testing.T) {
+		blob, err := ExportRSAPublicKeyBlob(&key.PublicKey, RSAKeyUsageKEYX)
+		require.NoError(t, err)
+
+		spew.Dump(blob)
+		require.Len(t, blob, 276)
+	})
+}
