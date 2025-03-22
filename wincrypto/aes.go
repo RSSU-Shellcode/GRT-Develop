8033aaf473d8d10582b8b2d8a2bc21ef352bfcf1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-03-22 20:17:43 +0800

    aes.go: implement AESEncrypt and AESDecrypt with CBC mode and PKCS#5.

--- wincrypto/aes.go
+++ wincrypto/aes.go
@@ -1,1 +1,85 @@
 package wincrypto
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"fmt"
+)
+
+// +---------+-------------+
+// |   IV    | cipher data |
+// +---------+-------------+
+// | 16 byte |     var     |
+// +---------+-------------+
+
+// about AES block and IV.
+const (
+	AESBlockSize = 16
+	AESIVSize    = 16
+)
+
+// errors about AESEncrypt and AESDecrypt.
+var (
+	ErrEmptyPlainData     = errors.New("empty aes plain data")
+	ErrEmptyCipherData    = errors.New("empty aes cipher data")
+	ErrInvalidCipherData  = errors.New("invalid aes cipher data")
+	ErrInvalidPaddingSize = errors.New("invalid aes padding size")
+)
+
+// AESEncrypt is used to encrypt data with CBC mode and PKCS#5 padding method.
+func AESEncrypt(data, key []byte) ([]byte, error) {
+	l := len(data)
+	if l < 1 {
+		return nil, ErrEmptyPlainData
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	// make buffer
+	paddingSize := AESBlockSize - l%AESBlockSize
+	output := make([]byte, AESIVSize+l+paddingSize)
+	// generate random iv
+	iv := output[:AESIVSize]
+	_, err = rand.Read(iv)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate aes iv: %s", err)
+	}
+	// encrypt plain data
+	encrypter := cipher.NewCBCEncrypter(block, iv) // #nosec G407
+	encrypter.CryptBlocks(output[AESIVSize:], output[AESIVSize:])
+	return output, nil
+}
+
+// AESDecrypt is used to decrypt data with CBC mode and PKCS#5 padding method.
+func AESDecrypt(data, key []byte) ([]byte, error) {
+	l := len(data)
+	if l < 1 {
+		return nil, ErrEmptyCipherData
+	}
+	if l-AESIVSize < AESBlockSize {
+		return nil, ErrInvalidCipherData
+	}
+	if (l-AESIVSize)%AESBlockSize != 0 {
+		return nil, ErrInvalidCipherData
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	output := make([]byte, l-AESIVSize)
+	iv := data[:AESIVSize]
+	// decrypt cipher data
+	decrypter := cipher.NewCBCDecrypter(block, iv)
+	decrypter.CryptBlocks(output, data[AESIVSize:])
+	// remove padding data
+	outputSize := len(output)
+	paddingSize := int(output[outputSize-1])
+	offset := outputSize - paddingSize
+	if offset < 0 {
+		return nil, ErrInvalidPaddingSize
+	}
+	return output[:offset], nil
+}
