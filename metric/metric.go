44b6ddd193f64ca3b8dde4fa130e05c13685bba5
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-11-16 13:27:51 +0800

    metric.go: move code about BOOL after type WDStatus struct.

--- metric/metric.go
+++ metric/metric.go
@@ -5,39 +5,6 @@ import (
 	"strings"
 )
 
-// BOOL is an int32 for structure align.
-type BOOL int32
-
-// ToBool is used to convert to go bool.
-func (b BOOL) ToBool() bool {
-	return b != 0
-}
-
-func (b BOOL) String() string {
-	if b.ToBool() {
-		return "true"
-	}
-	return "false"
-}
-
-// MarshalText is used to implement TextMarshaler interface.
-func (b BOOL) MarshalText() ([]byte, error) {
-	return []byte(b.String()), nil
-}
-
-// UnmarshalText is used to implement TextUnmarshaler interface.
-func (b *BOOL) UnmarshalText(data []byte) error {
-	switch strings.ToLower(string(data)) {
-	case "true":
-		*b = BOOL(1)
-	case "false":
-		*b = BOOL(0)
-	default:
-		return errors.New("invalid BOOL value")
-	}
-	return nil
-}
-
 // Metrics contains status about runtime submodules.
 type Metrics struct {
 	Library  LTStatus `toml:"library"  json:"library"`
@@ -113,4 +80,37 @@ type WDStatus struct {
 	NumKick   int64 `toml:"num_kick"   json:"num_kick"`
 	NumNormal int64 `toml:"num_normal" json:"num_normal"`
 	NumReset  int64 `toml:"num_reset"  json:"num_reset"`
+}
+
+// BOOL is an int32 for structure align.
+type BOOL int32
+
+// ToBool is used to convert to go bool.
+func (b BOOL) ToBool() bool {
+	return b != 0
+}
+
+func (b BOOL) String() string {
+	if b.ToBool() {
+		return "true"
+	}
+	return "false"
+}
+
+// MarshalText is used to implement TextMarshaler interface.
+func (b BOOL) MarshalText() ([]byte, error) {
+	return []byte(b.String()), nil
+}
+
+// UnmarshalText is used to implement TextUnmarshaler interface.
+func (b *BOOL) UnmarshalText(data []byte) error {
+	switch strings.ToLower(string(data)) {
+	case "true":
+		*b = BOOL(1)
+	case "false":
+		*b = BOOL(0)
+	default:
+		return errors.New("invalid BOOL value")
+	}
+	return nil
 }
