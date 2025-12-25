75be7ce0687a9248351b9f08f3e888e7f2b06b18
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-25 13:23:29 +0800

    metric.go: remove useless structure tag about toml in status.

--- metric/metric.go
+++ metric/metric.go
@@ -7,79 +7,79 @@ import (
 
 // Metrics contains status about runtime submodules.
 type Metrics struct {
-	Library  LTStatus `toml:"library"  json:"library"`
-	Memory   MTStatus `toml:"memory"   json:"memory"`
-	Thread   TTStatus `toml:"thread"   json:"thread"`
-	Resource RTStatus `toml:"resource" json:"resource"`
-	Detector DTStatus `toml:"detector" json:"detector"`
-	Watchdog WDStatus `toml:"watchdog" json:"watchdog"`
-	Sysmon   SMStatus `toml:"sysmon"   json:"sysmon"`
+	Library  LTStatus `json:"library"`
+	Memory   MTStatus `json:"memory"`
+	Thread   TTStatus `json:"thread"`
+	Resource RTStatus `json:"resource"`
+	Detector DTStatus `json:"detector"`
+	Watchdog WDStatus `json:"watchdog"`
+	Sysmon   SMStatus `json:"sysmon"`
 }
 
 // LTStatus contains status about library tracker.
 type LTStatus struct {
-	NumModules    int64 `toml:"num_modules"    json:"num_modules"`
-	NumProcedures int64 `toml:"num_procedures" json:"num_procedures"`
+	NumModules    int64 `json:"num_modules"`
+	NumProcedures int64 `json:"num_procedures"`
 }
 
 // MTStatus contains status about memory tracker.
 type MTStatus struct {
-	NumGlobals int64 `toml:"num_globals" json:"num_globals"`
-	NumLocals  int64 `toml:"num_locals"  json:"num_locals"`
-	NumBlocks  int64 `toml:"num_blocks"  json:"num_blocks"`
-	NumRegions int64 `toml:"num_regions" json:"num_regions"`
-	NumPages   int64 `toml:"num_pages"   json:"num_pages"`
-	NumHeaps   int64 `toml:"num_heaps"   json:"num_heaps"`
+	NumGlobals int64 `json:"num_globals"`
+	NumLocals  int64 `json:"num_locals"`
+	NumBlocks  int64 `json:"num_blocks"`
+	NumRegions int64 `json:"num_regions"`
+	NumPages   int64 `json:"num_pages"`
+	NumHeaps   int64 `json:"num_heaps"`
 }
 
 // TTStatus contains status about thread tracker.
 type TTStatus struct {
-	NumThreads  int64 `toml:"num_threads"   json:"num_threads"`
-	NumTLSIndex int64 `toml:"num_tls_index" json:"num_tls_index"`
-	NumSuspend  int64 `toml:"num_suspend"   json:"num_suspend"`
+	NumThreads  int64 `json:"num_threads"`
+	NumTLSIndex int64 `json:"num_tls_index"`
+	NumSuspend  int64 `json:"num_suspend"`
 }
 
 // RTStatus contains status about resource tracker.
 type RTStatus struct {
-	NumMutexs         int64 `toml:"num_mutexs"          json:"num_mutexs"`
-	NumEvents         int64 `toml:"num_events"          json:"num_events"`
-	NumSemaphores     int64 `toml:"num_semaphores"      json:"num_semaphores"`
-	NumWaitableTimers int64 `toml:"num_waitable_timers" json:"num_waitable_timers"`
-	NumFiles          int64 `toml:"num_files"           json:"num_files"`
-	NumDirectories    int64 `toml:"num_directories"     json:"num_directories"`
-	NumIOCPs          int64 `toml:"num_iocps"           json:"num_iocps"`
-	NumRegKeys        int64 `toml:"num_reg_keys"        json:"num_reg_keys"`
-	NumSockets        int64 `toml:"num_sockets"         json:"num_sockets"`
+	NumMutexs         int64 `json:"num_mutexs"`
+	NumEvents         int64 `json:"num_events"`
+	NumSemaphores     int64 `json:"num_semaphores"`
+	NumWaitableTimers int64 `json:"num_waitable_timers"`
+	NumFiles          int64 `json:"num_files"`
+	NumDirectories    int64 `json:"num_directories"`
+	NumIOCPs          int64 `json:"num_iocps"`
+	NumRegKeys        int64 `json:"num_reg_keys"`
+	NumSockets        int64 `json:"num_sockets"`
 }
 
 // DTStatus contains status about detector.
 type DTStatus struct {
-	IsEnabled        BOOL  `toml:"is_enabled"         json:"is_enabled"`
-	HasDebugger      BOOL  `toml:"has_debugger"       json:"has_debugger"`
-	HasMemoryScanner BOOL  `toml:"has_memory_scanner" json:"has_memory_scanner"`
-	InSandbox        BOOL  `toml:"in_sandbox"         json:"in_sandbox"`
-	InVirtualMachine BOOL  `toml:"in_virtual_machine" json:"in_virtual_machine"`
-	InEmulator       BOOL  `toml:"in_emulator"        json:"in_emulator"`
-	IsAccelerated    BOOL  `toml:"is_accelerated"     json:"is_accelerated"`
-	SafeRank         int32 `toml:"safe_rank"          json:"safe_rank"`
+	IsEnabled        BOOL  `json:"is_enabled"`
+	HasDebugger      BOOL  `json:"has_debugger"`
+	HasMemoryScanner BOOL  `json:"has_memory_scanner"`
+	InSandbox        BOOL  `json:"in_sandbox"`
+	InVirtualMachine BOOL  `json:"in_virtual_machine"`
+	InEmulator       BOOL  `json:"in_emulator"`
+	IsAccelerated    BOOL  `json:"is_accelerated"`
+	SafeRank         int32 `json:"safe_rank"`
 }
 
 // WDStatus contains status about watchdog.
 type WDStatus struct {
-	IsEnabled BOOL  `toml:"is_enabled" json:"is_enabled"`
-	Reserved  int32 `toml:"-"          json:"-"`
-	NumKick   int64 `toml:"num_kick"   json:"num_kick"`
-	NumNormal int64 `toml:"num_normal" json:"num_normal"`
-	NumReset  int64 `toml:"num_reset"  json:"num_reset"`
+	IsEnabled BOOL  `json:"is_enabled"`
+	Reserved  int32 `json:"-"`
+	NumKick   int64 `json:"num_kick"`
+	NumNormal int64 `json:"num_normal"`
+	NumReset  int64 `json:"num_reset"`
 }
 
 // SMStatus contains status about sysmon.
 type SMStatus struct {
-	IsEnabled  BOOL  `toml:"is_enabled"  json:"is_enabled"`
-	Reserved   int32 `toml:"-"           json:"-"`
-	NumNormal  int64 `toml:"num_normal"  json:"num_normal"`
-	NumRecover int64 `toml:"num_recover" json:"num_recover"`
-	NumPanic   int64 `toml:"num_panic"   json:"num_panic"`
+	IsEnabled  BOOL  `json:"is_enabled"`
+	Reserved   int32 `json:"-"`
+	NumNormal  int64 `json:"num_normal"`
+	NumRecover int64 `json:"num_recover"`
+	NumPanic   int64 `json:"num_panic"`
 }
 
 // BOOL is an int32 for structure align.
