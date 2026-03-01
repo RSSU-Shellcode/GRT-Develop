d3c9d308d635426d746f5d4912b914d5705e67ec
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-01 14:47:43 +0800

    shield.go: append Volatile and Nonvolatile register map.

--- shield/shield.go
+++ shield/shield.go
@@ -23,22 +23,29 @@ var (
 )
 
 var (
-	registerX86 = []string{
-		"eax", "ebx", "ecx", "edx",
-		"ebp", "edi", "esi",
+	regVolatileX86 = []string{
+		"eax", "ecx", "edx",
 	}
 
-	registerX64 = []string{
-		"rax", "rbx", "rcx", "rdx",
-		"rbp", "rdi", "rsi",
+	regNonvolatileX86 = []string{
+		"ebx", "ebp", "edi", "esi",
+	}
+
+	regVolatileX64 = []string{
+		"rax", "rcx", "rdx",
 		"r8", "r9", "r10", "r11",
+	}
+
+	regNonvolatileX64 = []string{
+		"rbx", "rbp", "rdi", "rsi",
 		"r12", "r13", "r14", "r15",
 	}
 )
 
 type shieldCtx struct {
 	// for replace registers
-	Reg map[string]string
+	RegV map[string]string
+	RegN map[string]string
 }
 
 func (gen *Generator) buildShield() ([]byte, error) {
@@ -50,7 +57,8 @@ func (gen *Generator) buildShield() ([]byte, error) {
 		shield = gen.getTemplateX64()
 	}
 	ctx := &shieldCtx{
-		Reg: gen.buildRandomRegisterMap(),
+		RegV: gen.buildVolatileRegisterMap(),
+		RegN: gen.buildNonvolatileRegisterMap(),
 	}
 	tpl, err := template.New("shield").Parse(shield)
 	if err != nil {
@@ -84,23 +92,47 @@ func (gen *Generator) getTemplateX64() string {
 	return defaultTemplateX64
 }
 
-func (gen *Generator) buildRandomRegisterMap() map[string]string {
+func (gen *Generator) buildVolatileRegisterMap() map[string]string {
 	var reg []string
 	switch gen.arch {
 	case 32:
-		reg = slices.Clone(registerX86)
+		reg = slices.Clone(regVolatileX86)
 	case 64:
-		reg = slices.Clone(registerX64)
+		reg = slices.Clone(regVolatileX64)
 	}
 	gen.regBox = reg
-	register := make(map[string]string, 16)
+	register := make(map[string]string, len(reg))
 	switch gen.arch {
 	case 32:
-		for _, reg := range registerX86 {
+		for _, reg := range regVolatileX86 {
 			register[reg] = gen.selectRegister()
 		}
 	case 64:
-		for _, reg := range registerX64 {
+		for _, reg := range regVolatileX64 {
+			register[reg] = gen.selectRegister()
+		}
+		gen.buildLowBitRegisterMap(register)
+	}
+	return register
+}
+
+func (gen *Generator) buildNonvolatileRegisterMap() map[string]string {
+	var reg []string
+	switch gen.arch {
+	case 32:
+		reg = slices.Clone(regNonvolatileX86)
+	case 64:
+		reg = slices.Clone(regNonvolatileX64)
+	}
+	gen.regBox = reg
+	register := make(map[string]string, len(reg))
+	switch gen.arch {
+	case 32:
+		for _, reg := range regNonvolatileX86 {
+			register[reg] = gen.selectRegister()
+		}
+	case 64:
+		for _, reg := range regNonvolatileX64 {
 			register[reg] = gen.selectRegister()
 		}
 		gen.buildLowBitRegisterMap(register)
