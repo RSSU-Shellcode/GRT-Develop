2c8e495d543042039d2196d3663dca7ee9d10932
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-01 15:19:56 +0800

    sc_*_test.go: rename argument about trap to proc.

--- shield/sc_windows_test.go
+++ shield/sc_windows_test.go
@@ -4,7 +4,7 @@ import (
 	"syscall"
 	"testing"
 	"unsafe"
-	
+
 	"github.com/stretchr/testify/require"
 	"golang.org/x/sys/windows"
 )
@@ -23,6 +23,6 @@ func loadShellcode(t *testing.T, sc []byte) uintptr {
 // for cross-compile
 //
 //go:uintptrescapes
-func syscallN(trap uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
-	return syscall.SyscallN(trap, args...)
+func syscallN(proc uintptr, args ...uintptr) (r1, r2 uintptr, err syscall.Errno) {
+	return syscall.SyscallN(proc, args...)
 }
