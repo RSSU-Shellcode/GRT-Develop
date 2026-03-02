014cfa110d5e3d48c04668ec02c893db15571002
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-02 12:38:38 +0800

    shield_test.go: add sub test about x86, finish testNewShieldArgs.

--- shield/shield_test.go
+++ shield/shield_test.go
@@ -1,9 +1,11 @@
 package shield
 
 import (
+	"runtime"
 	"strings"
 	"syscall"
 	"testing"
+	"time"
 	"unsafe"
 
 	"github.com/stretchr/testify/require"
@@ -28,12 +30,21 @@ type testShieldArgs struct {
 }
 
 func testNewShieldArgs(t *testing.T, critical []byte) *testShieldArgs {
+	hTimer, _, err := procCreateWaitableTimerA.Call(0, 0, 0)
+	if hTimer == 0 {
+		require.NoError(t, err)
+	}
+	dueTime := int64(-1000 * 1000 * 10) // 1s
+	ok, _, err := procSetWaitableTimer.Call(
+		hTimer, uintptr(unsafe.Pointer(&dueTime)), 0, 0, 0, 1,
+	)
+	require.True(t, ok == 1, err)
 	ctx := &testShieldArgs{
 		CriticalAddress:     uintptr(unsafe.Pointer(&critical[0])),
 		CriticalSize:        uintptr(len(critical)),
 		VirtualProtect:      procVirtualProtect.Addr(),
 		WaitForSingleObject: procWaitForSingleObject.Addr(),
-		Timer:               0, // TODO
+		Timer:               hTimer,
 		Key:                 0x12345678,
 	}
 	return ctx
@@ -46,21 +57,45 @@ func TestShield(t *testing.T) {
 	copy(critical, "runtime instruction")
 
 	t.Run("x86", func(t *testing.T) {
+		ctx, err := generator.Generate(32, nil)
+		require.NoError(t, err)
 
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		shield := loadShellcode(t, ctx.Output)
+		args := testNewShieldArgs(t, critical)
+
+		now := time.Now()
+
+		r1, _, _ := syscallN(shield, uintptr(unsafe.Pointer(args)))
+		require.Equal(t, uintptr(0x12345678), r1)
+
+		time.Since(now)
+
+		require.True(t, strings.HasPrefix(string(critical), "runtime instruction"))
 	})
 
 	t.Run("x64", func(t *testing.T) {
 		ctx, err := generator.Generate(64, nil)
 		require.NoError(t, err)
 
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
 		shield := loadShellcode(t, ctx.Output)
 		args := testNewShieldArgs(t, critical)
 
+		now := time.Now()
+
 		r1, _, _ := syscallN(shield, uintptr(unsafe.Pointer(args)))
 		require.Equal(t, uintptr(0x12345678), r1)
-	})
 
-	require.True(t, strings.HasPrefix(string(critical), "runtime instruction"))
+		time.Since(now)
+
+		require.True(t, strings.HasPrefix(string(critical), "runtime instruction"))
+	})
 
 	err := generator.Close()
 	require.NoError(t, err)
