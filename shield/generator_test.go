9014e980128b828fc98ccf8f3d23367f0fd6cf1a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-07 12:58:02 +0800

    generator_test.go: add TestSpecificSeed for compare output.

--- shield/generator_test.go
+++ shield/generator_test.go
@@ -46,3 +46,42 @@ func TestGenerator(t *testing.T) {
 	err := generator.Close()
 	require.NoError(t, err)
 }
+
+func TestSpecificSeed(t *testing.T) {
+	generator := NewGenerator()
+
+	opts := &Options{
+		RandSeed: 1234,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		ctx1, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		ctx2, err := generator.Generate(32, opts)
+		require.NoError(t, err)
+		require.Equal(t, ctx1, ctx2)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "386" {
+			return
+		}
+
+		testShield(t, ctx1.Output)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		ctx1, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		ctx2, err := generator.Generate(64, opts)
+		require.NoError(t, err)
+		require.Equal(t, ctx1, ctx2)
+
+		if runtime.GOOS != "windows" || runtime.GOARCH != "amd64" {
+			return
+		}
+
+		testShield(t, ctx1.Output)
+	})
+
+	err := generator.Close()
+	require.NoError(t, err)
+}
